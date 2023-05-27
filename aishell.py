fb1c9f954ba38a7e2894daf75e1c4f59cf88e6ce
yeyupiaoling <yeyupiaoling@foxmail.com>
2023-05-27 11:45:37 +0800

    修改数据集下载地址

--- aishell.py
+++ aishell.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 from utils.utils import download, unpack
 from utils.utils import add_arguments, print_arguments
 
-DATA_URL = 'https://openslr.magicdatatech.com/resources/33/data_aishell.tgz'
+DATA_URL = 'https://openslr.elda.org/resources/33/data_aishell.tgz'
 MD5_DATA = '2f494334227864a8a8fec932999db9d8'
 
 parser = argparse.ArgumentParser(description=__doc__)
